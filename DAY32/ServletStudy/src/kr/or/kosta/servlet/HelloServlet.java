c0d6edbdc8c0f92690d61f037fb29ddc8772c36d
00js00 <js0000322@red.ujaen.es>
2018-10-11 18:16:43 +0900

    day32

---
+++ DAY32/ServletStudy/src/kr/or/kosta/servlet/HelloServlet.java
@@ -0,0 +1,51 @@
+package kr.or.kosta.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 최초 작성 서블릿
+ * 
+ * @author 서지원
+ *
+ */
+
+public class HelloServlet extends HttpServlet /* implements Servlet */ {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 웹 클라이언트로 동적 출력하고자 하는 데이터 생성
+		//Calendar now = Calendar.getInstance();
+		String nowString = null;
+		//String nowString = String.format("%1$tF %1$tT", now);
+		nowString.length();
+		
+		//응답 메시지의 헤더에 컨텐츠 유형 설정 - 한글로 만들기
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		
+//		이렇게 할 수도 있음
+//		String html = "<html>";
+//				html += "</html>";
+//		
+		
+		out.println("<html>"); // html 을 보냄
+		out.println("<head>");
+		out.println("<title>Servlet Programming</title>");
+		out.println("<meta charset=\"utf-8\">");
+		out.println("</head>");
+		out.println("<body>");
+
+		out.println("<h2>오늘은 " + nowString + " 입니다..</h2>");
+
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
---
+++ DAY32/ServletStudy/src/kr/or/kosta/servlet/ReceiveServlet.java
@@ -0,0 +1,63 @@
+package kr.or.kosta.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Calendar;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 에코
+ */
+public class ReceiveServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		process(request, response);
+	}
+	public void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+
+		// 요청 파라메터 수신
+		String userId = request.getParameter("userid");
+		String userPw = request.getParameter("userpw");
+		String team = request.getParameter("teams");
+		String[] hobbys = request.getParameterValues("hobby"); // 선택안하면 null
+		Enumeration<String> paramNames = request.getParameterNames();
+
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Servlet Programming</title>");
+		out.println("<meta charset=\"utf-8\">");
+		out.println("</head>");
+		out.println("<body style='font-size: 20pt;'>");
+		out.println("<h3>아이디 : " + userId + "<h3/>"); // 전달되는게 없으면 null & 값 입력안된거 오면 빈문자
+		out.println("<h3>비번 : " + userPw + "<h3/>");
+		out.println("<h3>팀 : " + team + "<h3/>");
+		if (hobbys != null) {
+			for (String hobby : hobbys) {
+				out.println("<h3>취미 : " + hobby + "<h3/>");
+			}
+		}
+
+		while (paramNames.hasMoreElements()) {
+			String name = paramNames.nextElement();
+			String value = request.getParameter(name);
+			System.out.println(name + "=" + value);
+
+		}
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
